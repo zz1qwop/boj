2a184e8998c816ab45efb98d518a3a6578c8c138
Minji <zz1qwop@gmail.com>
2021-05-17 22:07:32 +0900

    Q1000

---
+++ Q1000.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q1000 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		System.out.println(x+y);
+
+	}
+
+}
